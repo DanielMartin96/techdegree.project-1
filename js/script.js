53d1ed638341d7a5af90bf23ef62eeb3e24386d1
DanielMartin96 <danielmartin1996@hotmail.com>
2020-08-23 15:02:34 +0100

    final edit

--- js/script.js
+++ js/script.js
@@ -3,14 +3,6 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
 let quotes = [
   {
     quote: "Shaken, not stirred",
@@ -44,69 +36,40 @@ let quotes = [
   },
 ];
 
-/***
- * `getRandomQuote` function
-***/
-
 function getRandomQuote(array) {
+  let fullArray = [];
   const randomNumber = Math.floor(Math.random() * array.length);
-  const randomQuote = array[randomNumber].quote;
-  return randomQuote;
+  fullArray.push(array[randomNumber].quote);
+  fullArray.push(array[randomNumber].source);
+  fullArray.push(array[randomNumber].citation);
+  fullArray.push(array[randomNumber].year);
+  return fullArray;
 }
 
-const letRandomQuote = getRandomQuote(quotes);
-  let html = `<p class="quotes">${letRandomQuote}</p><p class="source">${letRandomQuote.source}</p>`;
-  document.getElementById('quote-box').innerHTML = html;
-
-  // 1. Create a variable that generates a random number
-  // between zero and the last index in the `quotes` array
-  // 2. Use the random number variable and bracket notation 
-  // to grab a random object from the `quotes` array, and 
-  // store it in a variable
-  // 3. Return the variable storing the random quote object
-
-/***
- * `printQuote` function
-***/
-
-  // 1. Create a variable that calls the getRandomQuote() 
-  // function
+fullFullArray = getRandomQuote(quotes);
+const quote = fullFullArray[0];
+const source = fullFullArray[1];
+const citation = fullFullArray[2];
+const year = fullFullArray[3];
 
 function printQuote() {
 
-  // 2. Create a variable that initiates your HTML string with 
-  // the first two <p></p> elements, their classNames, 
-  // and the quote and source properties, but leave off 
-  // the second closing `</p>` tag for now
+fullFullArray = getRandomQuote(quotes);
+const quote = fullFullArray[0];
+const source = fullFullArray[1];
+const citation = fullFullArray[2];
+const year = fullFullArray[3];
 
-  const letRandomQuote = getRandomQuote(quotes);
-  let html = `<p class="quotes">${letRandomQuote}</p><p class="source">${letRandomQuote.source}</p>`;
+let html = `<p class="quotes">${quote}</p><p class="source">${source}`
+  if (citation != '') {
+    html += `<span class="citation">${citation}</span>`
+  }
+  if (year != '') {
+    html += `<span class="year">${year}</span>`
+  }
+  html += `</p>`;
 
-  // 3. Use an if statement to check if the citation property 
-  // exists, and if it does, concatenate a <span></span> 
-  // element, appropriate className, and citation property 
-  // to the HTML string
-
-  // 4. Use an if statement to check of the year property exists, 
-  // and if it does, concatenate a <span></span> element, 
-  // appropriate className, and year property to the HTML 
-  //string
-
-  // 5. After the two if statements, concatenate the closing </p> 
-  // tag to the HTML string
-
-  // 6. set the innerHTML of the quote-box div to equal the 
-  // complete HTML string
-
-  document.getElementById('quote-box').innerHTML = html;
-
+document.getElementById('quote-box').innerHTML = html;
 }
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
