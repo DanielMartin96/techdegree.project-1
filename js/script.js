ff186b3b3e946551e494c781e0ed7a0b38a6b8ec
DanielMartin96 <danielmartin1996@hotmail.com>
2020-08-23 16:28:40 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,44 +36,6 @@ let quotes = [
   },
 ];
 
-/* function getRandomQuote(array) {
-  let fullArray = [];
-  const randomNumber = Math.floor(Math.random() * array.length);
-  fullArray.push(array[randomNumber].quote);
-  fullArray.push(array[randomNumber].source);
-  fullArray.push(array[randomNumber].citation);
-  fullArray.push(array[randomNumber].year);
-  return fullArray;
-}
-
-fullFullArray = getRandomQuote(quotes);
-const quote = fullFullArray[0];
-const source = fullFullArray[1];
-const citation = fullFullArray[2];
-const year = fullFullArray[3];
-
-function printQuote() {
-
-fullFullArray = getRandomQuote(quotes);
-const quote = fullFullArray[0];
-const source = fullFullArray[1];
-const citation = fullFullArray[2];
-const year = fullFullArray[3];
-
-let html = `<p class="quotes">${quote}</p><p class="source">${source}`
-  if (citation != '') {
-    html += `<span class="citation">${citation}</span>`
-  }
-  if (year != '') {
-    html += `<span class="year">${year}</span>`
-  }
-  html += `</p>`;
-
-document.getElementById('quote-box').innerHTML = html;
-}
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false); */
-
 function getRandomQuote(array) {
   const randomNumber = Math.floor(Math.random() * array.length)
   const randomQuote = array[randomNumber];
